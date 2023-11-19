c8b87c202fa86d2d8c6834aa09cde45510de395e
redoste <redoste@redoste.xyz>
2023-11-19 02:19:47 +0100

    tests : Fix the given `add-simple` test which expect sp to be set

--- tests/add-simple.s
+++ tests/add-simple.s
@@ -2,6 +2,11 @@ addi a1, zero, 20
 addi a2, zero, 22
 add a0, a1, a2
 
+# TODO : what's the initial value for sp ?
+#        in my opinion it's up to the guest to setup all of its registers
+#        except for pc (assuming it knows the memory map)
+li sp, 0x40
+slli sp, sp, 8
 
 # EXPECTED
 # sp: 16384
